58fdf0e8a3785f37a61b51efd569a4112f60fbd5
amirmorsy <amirahmedmorsy2019@gmail.com>
2024-08-10 02:21:25 +0300

    Initial Commit

---
+++ MobileDeveloperChallenge/MobileDeveloperChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDeveloperChallengeApp.swift
+//  MobileDeveloperChallenge
+//
+//  Created by Amir Ahmed on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MobileDeveloperChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
